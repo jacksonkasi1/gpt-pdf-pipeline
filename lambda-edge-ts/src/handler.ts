b98aac7e272ac6b58f86e5962e797c7a8b9a522a
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-09-06 19:59:44 +0530

    intial commit

---
+++ lambda-edge-ts/src/handler.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { handle } from "hono/aws-lambda";
+
+// ** import middlewares
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono({ strict: false });
+
+/**
+ * Middlewares
+ * https://hono.dev/middleware/builtin/cors
+ */
+app.use("*", logger());
+app.use("*", cors());
+
+/**
+ * Ping Pong
+ */
+app.use("/", async (c) => c.text("Hello World "));
+app.get("/ping", (c) => c.json({ ping: "pong" }, 200));
+
+const port = process.env.PORT || 3000;
+console.log(`Starting server on port http://127.0.0.1:${port} 🚀`);
+
+export const handler = handle(app);
