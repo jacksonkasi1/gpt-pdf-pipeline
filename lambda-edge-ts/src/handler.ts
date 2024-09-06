d83bb6d59289644fbdf581ccd038880d259ad74c
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-09-06 20:45:40 +0530

    modify port

--- lambda-edge-ts/src/handler.ts
+++ lambda-edge-ts/src/handler.ts
@@ -17,10 +17,10 @@ app.use("*", cors());
 /**
  * Ping Pong
  */
-app.use("/", async (c) => c.text(`Hello World ^ version = ${process.env.NODE_ENV} ` ));
+app.use("/", async (c) => c.text(`Hello World ^ version = ${process.env.NODE_ENV} `));
 app.get("/ping", (c) => c.json({ ping: "pong" }, 200));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 console.log(`Starting server on port http://127.0.0.1:${port} 🚀`);
 
 export const handler = handle(app);
